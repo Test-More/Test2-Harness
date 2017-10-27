a94c2b09f45cbc69afa0fb85af2c20e7f1ae899c
Chad Granum <exodist7@gmail.com>
2017-10-27 12:11:27 -0700

    Add a test helper for harness commands

---
+++ lib/Test2/Tools/HarnessTester.pm
@@ -0,0 +1,41 @@
+package Test2::Tools::HarnessTester;
+use strict;
+use warnings;
+
+use IPC::Open3 qw/open3/;
+use Test2::Harness::Util qw/open_file/;
+use List::Util qw/first/;
+
+use Importer Importer => 'import';
+
+our @EXPORT_OK = qw/run_yath_command/;
+
+my ($YATH) = first { -x $_ } 'scripts/yath', '../scripts/yath';
+$YATH ||= do {
+    require App::Yath::Util;
+    App::Yath::Util::find_yath();
+};
+
+sub run_yath_command {
+    my ($cmd, @args) = @_;
+
+    pipe(my($r_out, $w_out)) or die "Could not open pipe for STDOUT: $!";
+    pipe(my($r_err, $w_err)) or die "Could not open pipe for STDERR: $!";
+
+    my $pid = open3(undef, '>&' . fileno($w_out), '>&' . fileno($w_err), $^X, $YATH, $cmd, @args);
+    close($w_out);
+    close($w_err);
+
+    my $ret = waitpid($pid, 0);
+    my $exit = $?;
+
+    die "Error waiting on child process" unless $ret == $pid;
+
+    return {
+        exit => $exit,
+        stdout => join("" => <$r_out>),
+        stderr => join("" => <$r_err>),
+    };
+}
+
+1;

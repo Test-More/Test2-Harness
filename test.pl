557491543ea81e5f9ee449e2d00fe134da6443cf
Chad Granum <exodist7@gmail.com>
2017-11-01 10:17:10 -0700

    Generate (and slightly modify) test.pl
    test.pl formerly was used to test several commands, now it is simply a
    runner.

--- test.pl
+++ test.pl
@@ -4,32 +4,13 @@
 use strict;
 use warnings;
 
-use Test2::Tools::Tiny;
+system($^X, '-Ilib', './scripts/yath', 'test', '--default-search' => './t', '--default-search' => './t2', @ARGV);
+my $exit = $?;
 
-my @search = ('t', 't2');
-push @search => 'xt' if $ENV{AUTHOR_TESTING};
+# This makes sure it works with prove.
+print "1..1\n";
+print "not " if $exit;
+print "ok 1 - Passed tests when run by yath\n";
+print STDERR "yath exited with $exit" if $exit;
 
-system($^X, '-Ilib', './scripts/yath', 'test', '-j2', @search);
-my $exit1 = $?;
-ok(!$exit1, "Passed tests when run by yath", "`yath test -j2` exited with $exit1");
-
-system($^X, '-Ilib', './scripts/yath', 'test', '-j2', '--no-fork', @search);
-my $exit2 ||= $?;
-ok(!$exit2, "Passed tests when run by yath", "`yath test -j2 --no-fork` exited with $exit2");
-
-system($^X, '-Ilib', './scripts/yath', 'start', '-j2');
-my $exit3 ||= $?;
-ok(!$exit3, "started a persistant yath", "`yath -j2 --no-fork` exited with $exit3");
-
-system($^X, '-Ilib', './scripts/yath', 'run', @search);
-my $exit4 ||= $?;
-ok(!$exit4, "Tests passed", "`yath run` exited with $exit4");
-
-system($^X, '-Ilib', './scripts/yath', 'stop');
-my $exit5 ||= $?;
-ok(!$exit5, "stopped a persistant yath", "`yath -j2 --no-fork` exited with $exit5");
-
-done_testing;
-
-# Normalize the exit value
-exit(($exit1 || $exit2)? 255 : 0);
+exit($exit ? 255 : 0);

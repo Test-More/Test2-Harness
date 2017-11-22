c2d3b84746ced5e7069da675562ee7ae9c278aba
Chad Granum <exodist7@gmail.com>
2017-11-22 09:59:04 -0800

    Fix infinite recursion issue.
    Test fix for #46

--- lib/App/Yath/Util.pm
+++ lib/App/Yath/Util.pm
@@ -42,7 +42,7 @@ sub find_in_updir {
     while(1) {
         $path = File::Spec->catdir('..', $path);
         my $check = eval { realpath(File::Spec->rel2abs($path)) };
-        next unless $check;
+        last unless $check;
         last if $seen{$check}++;
         return $check if -f $check;
     }

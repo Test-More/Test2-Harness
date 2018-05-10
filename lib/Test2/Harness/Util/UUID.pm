61d3d8dae7ebc481665f15bdfe411baa07a023b9
Todd Rinaldo <toddr@cpanel.net>
2018-05-09 23:43:05 -0500

    Revert "Switch Test2::Harness::Util::UUID to use a more random UUID"
    This reverts commit b1ccece913fc49f79c79904a4cd526b0837cc1ca.
    
    This was causing a hang on some tests because Dev::Urandom was holding the file
    handle open to /dev/urandom permanently. This was somehow breaking on forks.
    I'm going to work with the author and may re-visit this idea later.
    
    See also https://github.com/Test-More/Test2-Harness/issues/70

--- lib/Test2/Harness/Util/UUID.pm
+++ lib/Test2/Harness/Util/UUID.pm
@@ -4,23 +4,12 @@ use warnings;
 
 our $VERSION = '0.001066';
 
-use Crypt::URandom;
-
+use Data::UUID;
 use Importer 'Importer' => 'import';
 
 our @EXPORT = qw/gen_uuid/;
 
-sub random_uuid_binary {
-    my $bytes = Crypt::URandom::urandom(16);
-
-    # Setting these bits this way is required by RFC 4122.
-    vec($bytes, 35, 2) = 0x2;
-    vec($bytes, 13, 4) = 0x4;
-    return $bytes;
-}
-
-sub gen_uuid() {
-    return uc join '-', unpack('H8 H4 H4 H4 H12', random_uuid_binary());
-}
+my $UG = Data::UUID->new;
+sub gen_uuid() { $UG->create_str() }
 
 1;

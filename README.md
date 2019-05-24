7adb9ff264c66fe72d6b1378c8c373832fb989d8
Todd Rinaldo <toddr@cpanel.net>
2019-05-24 17:09:59 -0500

    Update POD and create a readme.md

---
+++ README.md
@@ -0,0 +1,75 @@
+# NAME
+
+Test2::Harness::Renderer::JUnit - Captures Test2::Harness results and emits a junit xml file.
+
+# SYNOPSIS
+
+On the command line, with `yath`:
+
+    JUNIT_TEST_FILE="/tmp/foo.xml" ALLOW_PASSING_TODOS=1 yath test -j18 t/*.t
+
+# DESCRIPTION
+
+`Test2::Harness::Renderer::JUnit` provides JUnit output formatting sufficient
+to be parsed by Jenkins and hopefully other junit parsers.
+
+This code borrows many ideas from `TAP::Formatter::JUnit` but unlike that module
+does not provide a method to emit a different xml file for every testcase.
+Instead, it defaults to emitting to a single **junit.xml** to whatever the directory
+was you were in when you ran yath. This can be overridden by setting the
+`JUNIT_TEST_FILE` environment variable
+
+Timing information is included in the JUnit XML since this is native to `Test2::Harness`
+
+In standard use, "passing TODOs" are treated as failure conditions (and are
+reported as such in the generated JUnit).  If you wish to treat these as a
+"pass" and not a "fail" condition, setting `ALLOW_PASSING_TODOS=1` in your
+environment will turn these into pass conditions.
+
+The JUnit output generated was developed to be used by Jenkins
+([https://jenkins.io/](https://jenkins.io/)).  That's the build tool we use at the
+moment and needed to be able to generate JUnit output for.
+
+# METHODS
+
+- **render\_event($event)**
+
+    This is the only method (other than finish) that is called by Test2::Harness in order to
+    gather the data needed to emit the needed xml.
+
+- **close\_open\_failure\_testcase($test, $new\_test\_number)**
+
+    This method is called whenever a new test result or the end of a run is seen. Because
+    we want to capture test diag messages after a failed test, we delay emitting a failure
+    until we see the end of the testcase or until we see a new test number.
+
+- **finish()**
+
+    This method is called by Test2::Harness when all runs are complete. It takes what has
+    been gathered to that point and creates the junit xml file.
+
+- xml
+
+    An `XML::Generator` instance, to be used to generate XML output.
+
+# SOURCE
+
+The source code repository for Test2-Harness-Renderer-JUnit can be found at
+`https://github.com/CpanelInc/Test2-Harness-Renderer-JUnit`.
+
+# MAINTAINERS
+
+- Todd Rinaldo &lt;lt>toddr@cpanel.net>
+
+# AUTHORS
+
+- Todd Rinaldo &lt;lt>toddr@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2019 Todd Rinaldo&lt;lt>toddr@cpanel.net>.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+See `http://dev.perl.org/licenses/`
--- lib/Test2/Harness/Renderer/JUnit.pm
+++ lib/Test2/Harness/Renderer/JUnit.pm
@@ -2,38 +2,32 @@ package Test2::Harness::Renderer::JUnit;
 use strict;
 use warnings;
 
-our $VERSION = '0.001074';
+our $VERSION = '0.001077';
 
-use Carp qw/croak/;
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
+# This is used frequently during development to determine what different events look like so we can determine how to capture test data.
+#use Data::Dumper; $Data::Dumper::Sortkeys = 1;
 
 use File::Spec;
-
 use Storable qw/dclone/;
 use XML::Generator ();
 
-use Test2::Util qw/pkg_to_file/;
-use Test2::Harness::Util qw/fqmod/;
-use Test2::Harness::Util::JSON qw/encode_pretty_json/;
-
 BEGIN { require Test2::Harness::Renderer; our @ISA = ('Test2::Harness::Renderer') }
 use Test2::Harness::Util::HashBase qw{
-    -io -io_err
-    -formatter
-    -show_run_info
-    -show_job_info
-    -show_job_launch
-    -show_job_end
-    -times
+  -io -io_err
+  -formatter
+  -show_run_info
+  -show_job_info
+  -show_job_launch
+  -show_job_end
+  -times
 };
 
 sub init {
     my $self = shift;
 
-    my $settings = $self->{+SETTINGS};
+    my $settings = $self->{ +SETTINGS };
 
-    $self->{'xml'} = XML::Generator->new(':pretty', ':std', 'escape' => 'always,high-bit,even-entities', 'encoding' => 'UTF-8');
+    $self->{'xml'} = XML::Generator->new( ':pretty', ':std', 'escape' => 'always,high-bit,even-entities', 'encoding' => 'UTF-8' );
 
     $self->{'xml_content'} = [];
 
@@ -42,6 +36,8 @@ sub init {
 
     $self->{'tests'} = {};    # We need a pointer to each test so we know where to go for each event.
 }
+
+# This sub is called for every Harness event. We capture the data we need so we can emit the appropriate junit file.
 
 sub render_event {
     my $self = shift;
@@ -57,12 +53,12 @@ sub render_event {
 
     # At job launch we need to start collecting a new junit testdata section.
     # We throw out anything we've collected to date.
-    if ($f->{'harness_job_launch'}) {
+    if ( $f->{'harness_job_launch'} ) {
         my $full_test_name = $job->{'file'};
         my $test_file      = File::Spec->abs2rel($full_test_name);
 
         # Purge any previous runs of this job if we're seeing a new one starting.
-        foreach my $id (keys %{$self->{'tests'}}) {
+        foreach my $id ( keys %{ $self->{'tests'} } ) {
             delete $self->{'tests'}->{$id} if $self->{'tests'}->{$id}->{name} eq $full_test_name;
         }
 
@@ -89,20 +85,20 @@ sub render_event {
     my $test = $self->{'tests'}->{$job_id};
 
     # We have all the data. Print the XML.
-    if ($f->{'harness_job_end'}) {
-        $self->close_open_failure_testcase($test, -1);
+    if ( $f->{'harness_job_end'} ) {
+        $self->close_open_failure_testcase( $test, -1 );
         $test->{'stop'} = $event->{'stamp'};
         $test->{'testsuite'}->{'time'} = $test->{'stop'} - $test->{'start'};
 
-        push @{$test->{'testcase'}}, $self->xml->testcase({'name' => "Tear down.", 'time' => $stamp - $test->{'last_job_start'}}, "");
+        push @{ $test->{'testcase'} }, $self->xml->testcase( { 'name' => "Tear down.", 'time' => $stamp - $test->{'last_job_start'} }, "" );
 
-        if ($f->{'errors'}) {
+        if ( $f->{'errors'} ) {
             $test->{'error-msg'} //= '';
-            foreach my $msg (@{$f->{'errors'}}) {
+            foreach my $msg ( @{ $f->{'errors'} } ) {
                 next unless $msg->{'from_harness'};
                 next unless $msg->{'tag'} // '' eq 'REASON';
 
-                if ($msg->{details} =~ m/^Planned for ([0-9]+) assertions?, but saw ([0-9]+)/) {
+                if ( $msg->{details} =~ m/^Planned for ([0-9]+) assertions?, but saw ([0-9]+)/ ) {
                     $test->{'testsuite'}->{'errors'} += $1 - $2;
                 }
 
@@ -113,19 +109,19 @@ sub render_event {
         return;
     }
 
-    if ($f->{'plan'}) {
-        if ($f->{'plan'}->{'skip'}) {
+    if ( $f->{'plan'} ) {
+        if ( $f->{'plan'}->{'skip'} ) {
             my $skip = $f->{'plan'}->{'details'};
             $test->{'system-out'} .= "# SKIP $skip\n";
         }
-        if ($f->{'plan'}->{'count'}) {
+        if ( $f->{'plan'}->{'count'} ) {
             $test->{'plan'} = $f->{'plan'}->{'count'};
         }
 
         return;
     }
 
-    if ($f->{'harness_job_exit'}) {
+    if ( $f->{'harness_job_exit'} ) {
         return unless $f->{'harness_job_exit'}->{'exit'};
 
         # If we don't see
@@ -136,51 +132,52 @@ sub render_event {
     }
 
     # We just hit an ok/not ok line.
-    if ($f->{'assert'}) {
-        # Ignore subtests
-        return if ($f->{'hubs'} && $f->{'hubs'}->[0]->{'nested'});
+    if ( $f->{'assert'} ) {
 
-        my $test_num    = sprintf("%04d", $event->{'assert_count'} || $f->{'assert'}->{'number'} || die Dumper $event);
-        my $test_name   = _squeaky_clean($f->{'assert'}->{'details'} // 'UNKNOWN_TEST?');
+        # Ignore subtests
+        return if ( $f->{'hubs'} && $f->{'hubs'}->[0]->{'nested'} );
+
+        my $test_num    = sprintf( "%04d", $event->{'assert_count'} || $f->{'assert'}->{'number'} || die Dumper $event);
+        my $test_name   = _squeaky_clean( $f->{'assert'}->{'details'} // 'UNKNOWN_TEST?' );
         my $test_string = defined $test_name ? "$test_num - $test_name" : $test_num;
         $test->{'testsuite'}->{'tests'}++;
 
-        $self->close_open_failure_testcase($test, $test_num);
+        $self->close_open_failure_testcase( $test, $test_num );
 
         warn Dumper $event unless $stamp;
 
         my $run_time = $stamp - $test->{'last_job_start'};
         $test->{'last_job_start'} = $stamp;
 
-        if ($f->{'amnesty'} && grep { ($_->{'tag'} // '') eq 'TODO' } @{$f->{'amnesty'}}) {    # All TODO Tests
-            if (!$f->{'assert'}->{'pass'}) {                                                   # Failing TODO
-                push @{$test->{'testcase'}}, $self->xml->testcase({'name' => "$test_string (TODO)", 'time' => $run_time}, "");
+        if ( $f->{'amnesty'} && grep { ( $_->{'tag'} // '' ) eq 'TODO' } @{ $f->{'amnesty'} } ) {    # All TODO Tests
+            if ( !$f->{'assert'}->{'pass'} ) {                                                       # Failing TODO
+                push @{ $test->{'testcase'} }, $self->xml->testcase( { 'name' => "$test_string (TODO)", 'time' => $run_time }, "" );
             }
-            elsif ($self->{'allow_passing_todos'}) {                                           # junit parsers don't like passing TODO tests. Let's just not tell them about it if $ENV{ALLOW_PASSING_TODOS} is set.
-                push @{$test->{'testcase'}}, $self->xml->testcase({'name' => "$test_string (PASSING TODO)", 'time' => $run_time}, "");
+            elsif ( $self->{'allow_passing_todos'} ) {                                               # junit parsers don't like passing TODO tests. Let's just not tell them about it if $ENV{ALLOW_PASSING_TODOS} is set.
+                push @{ $test->{'testcase'} }, $self->xml->testcase( { 'name' => "$test_string (PASSING TODO)", 'time' => $run_time }, "" );
             }
-            else {                                                                             # Passing TODO (Failure) when not allowed.
+            else {                                                                                   # Passing TODO (Failure) when not allowed.
 
                 $test->{'testsuite'}->{'failures'}++;
                 $test->{'testsuite'}->{'errors'}++;
 
                 # Grab the first amnesty description that's a TODO message.
-                my ($todo_message) = map { $_->{'details'} } grep { $_->{'tag'} // '' eq 'TODO' } @{$f->{'amnesty'}};
+                my ($todo_message) = map { $_->{'details'} } grep { $_->{'tag'} // '' eq 'TODO' } @{ $f->{'amnesty'} };
 
-                push @{$test->{'testcase'}}, $self->xml->testcase(
-                    {'name' => "$test_string (TODO)", 'time' => $run_time},
+                push @{ $test->{'testcase'} }, $self->xml->testcase(
+                    { 'name' => "$test_string (TODO)", 'time' => $run_time },
                     $self->xml->error(
-                        {'message' => $todo_message, 'type' => "TodoTestSucceeded"},
+                        { 'message' => $todo_message, 'type' => "TodoTestSucceeded" },
                         $self->_cdata("ok $test_string")
                     )
                 );
 
             }
         }
-        elsif ($f->{'assert'}->{'pass'}) {    # Passing test
-            push @{$test->{'testcase'}}, $self->xml->testcase({'name' => "$test_string", 'time' => $run_time}, "");
+        elsif ( $f->{'assert'}->{'pass'} ) {    # Passing test
+            push @{ $test->{'testcase'} }, $self->xml->testcase( { 'name' => "$test_string", 'time' => $run_time }, "" );
         }
-        else {                                # Failing Test.
+        else {                                  # Failing Test.
             $test->{'testsuite'}->{'failures'}++;
             $test->{'testsuite'}->{'errors'}++;
 
@@ -196,8 +193,8 @@ sub render_event {
         return;
     }
 
-    if ($f->{'info'} && $test->{'last_failure'}) {
-        foreach my $line (@{$f->{'info'}}) {
+    if ( $f->{'info'} && $test->{'last_failure'} ) {
+        foreach my $line ( @{ $f->{'info'} } ) {
             next unless $line->{'details'};
             chomp $line->{'details'};
             $test->{'last_failure'}->{'message'} .= "# $line->{details}\n";
@@ -207,13 +204,12 @@ sub render_event {
 
 }
 
-# This is called when the last run is complete (allows for multiple retries),
-# As a result, we seek to the top of the file and truncate it every time so that new results that were updated on the re-run are re-emitted to the file.
-# We rely on destruction of this object to close the file handle in the end.
+# This is called when the last run is complete and we're ready to emit the junit file.
+
 sub finish {
     my $self = shift;
 
-    open(my $fh, '>', $self->{'junit_file'}) or die("Can't open '$self->{junit_file}' ($!)");
+    open( my $fh, '>', $self->{'junit_file'} ) or die("Can't open '$self->{junit_file}' ($!)");
 
     my $xml = $self->xml;
 
@@ -225,13 +221,13 @@ sub finish {
         map {
             $xml->testsuite(
                 $_->{'testsuite'},
-                @{$_->{'testcase'}},
-                $xml->$out_method($self->_cdata($_->{$out_method})),
-                $xml->$err_method($self->_cdata($_->{$err_method})),
-                $_->{'error-msg'} ? $xml->error({'message' => $_->{'error-msg'}}) : (),
-                )
-            }
-            sort { $a->{'job_name'} <=> $b->{'job_name'} } values %{$self->{'tests'}}
+                @{ $_->{'testcase'} },
+                $xml->$out_method( $self->_cdata( $_->{$out_method} ) ),
+                $xml->$err_method( $self->_cdata( $_->{$err_method} ) ),
+                $_->{'error-msg'} ? $xml->error( { 'message' => $_->{'error-msg'} } ) : (),
+              )
+          }
+          sort { $a->{'job_name'} <=> $b->{'job_name'} } values %{ $self->{'tests'} }
     ) . "\n";
 
     close $fh;
@@ -239,8 +235,11 @@ sub finish {
     return;
 }
 
+# Because we want to test diag messages after a failed test, we delay closing failures
+# until we see the end of the testcase or until we see a new test number.
+
 sub close_open_failure_testcase {
-    my ($self, $test, $new_test_number) = @_;
+    my ( $self, $test, $new_test_number ) = @_;
 
     # Need to handle failed TODOs
 
@@ -248,16 +247,16 @@ sub close_open_failure_testcase {
     return unless $test->{'last_failure'};
 
     my $fail = $test->{'last_failure'};
-    if ($fail->{'test_num'} == $new_test_number) {
+    if ( $fail->{'test_num'} == $new_test_number ) {
         die("The same assert number ($new_test_number) was seen twice for $test->{name}");
     }
 
     my $xml = $self->xml;
-    push @{$test->{'testcase'}}, $xml->testcase(
-        {'name' => "$fail->{test_num} - $fail->{test_name}", 'time' => $fail->{'time'}},
+    push @{ $test->{'testcase'} }, $xml->testcase(
+        { 'name' => "$fail->{test_num} - $fail->{test_name}", 'time' => $fail->{'time'} },
         $xml->failure(
-            {'message' => "not ok $fail->{test_num} - $fail->{test_name}", 'type' => 'TestFailed'},
-            $self->_cdata($fail->{'message'})
+            { 'message' => "not ok $fail->{test_num} - $fail->{test_name}", 'type' => 'TestFailed' },
+            $self->_cdata( $fail->{'message'} )
         )
     );
 
@@ -269,6 +268,8 @@ sub xml {
     my $self = shift;
     return $self->{'xml'};
 }
+
+# These helpers were borrowed from https://metacpan.org/pod/TAP::Formatter::JUnit. Thanks!
 
 ###############################################################################
 # Generates the name for the entire test suite.
@@ -292,20 +293,22 @@ sub _clean_to_java_class_name {
 # Creates a CDATA block for the given data (which is made squeaky clean first,
 # so that JUnit parsers like Hudson's don't choke).
 sub _cdata {
-    my ($self, $data) = @_;
+    my ( $self, $data ) = @_;
 
     # When I first added this conditional, I returned $data and at one point it was returning ^A and breaking the xml parser.
-    return '' if (!$data or $data !~ m/\S/ms);
+    return '' if ( !$data or $data !~ m/\S/ms );
 
-    return $self->xml->xmlcdata(_squeaky_clean($data));
+    return $self->xml->xmlcdata( _squeaky_clean($data) );
 }
 
 ###############################################################################
 # Clean a string to the point that JUnit can't possibly have a problem with it.
 sub _squeaky_clean {
     my $string = shift;
+
     # control characters (except CR and LF)
     $string =~ s/([\x00-\x09\x0b\x0c\x0e-\x1f])/"^".chr(ord($1)+64)/ge;
+
     # high-byte characters
     $string =~ s/([\x7f-\xff])/'[\\x'.sprintf('%02x',ord($1)).']'/ge;
     return $string;
@@ -321,20 +324,72 @@ __END__
 
 =head1 NAME
 
-Test2::Harness::Renderer::JUnit - writes a junit.xml file
+Test2::Harness::Renderer::JUnit - Captures Test2::Harness results and emits a junit xml file.
+
+=head1 SYNOPSIS
+
+On the command line, with F<yath>:
+
+  JUNIT_TEST_FILE="/tmp/foo.xml" ALLOW_PASSING_TODOS=1 yath test -j18 t/*.t
 
 =head1 DESCRIPTION
+
+C<Test2::Harness::Renderer::JUnit> provides JUnit output formatting sufficient
+to be parsed by Jenkins and hopefully other junit parsers.
+
+This code borrows many ideas from C<TAP::Formatter::JUnit> but unlike that module
+does not provide a method to emit a different xml file for every testcase.
+Instead, it defaults to emitting to a single B<junit.xml> to whatever the directory
+was you were in when you ran yath. This can be overridden by setting the
+C<JUNIT_TEST_FILE> environment variable
+
+Timing information is included in the JUnit XML since this is native to C<Test2::Harness>
+
+In standard use, "passing TODOs" are treated as failure conditions (and are
+reported as such in the generated JUnit).  If you wish to treat these as a
+"pass" and not a "fail" condition, setting C<ALLOW_PASSING_TODOS=1> in your
+environment will turn these into pass conditions.
+
+The JUnit output generated was developed to be used by Jenkins
+(L<https://jenkins.io/>).  That's the build tool we use at the
+moment and needed to be able to generate JUnit output for.
+
+=head1 METHODS
+
+=over
+
+=item B<render_event($event)>
+
+This is the only method (other than finish) that is called by Test2::Harness in order to
+gather the data needed to emit the needed xml.
+
+=item B<close_open_failure_testcase($test, $new_test_number)>
+
+This method is called whenever a new test result or the end of a run is seen. Because
+we want to capture test diag messages after a failed test, we delay emitting a failure
+until we see the end of the testcase or until we see a new test number.
+
+=item B<finish()>
+
+This method is called by Test2::Harness when all runs are complete. It takes what has
+been gathered to that point and creates the junit xml file.
+
+=item xml
+
+An C<XML::Generator> instance, to be used to generate XML output.
+
+=back
 
 =head1 SOURCE
 
 The source code repository for Test2-Harness-Renderer-JUnit can be found at
-F<http://github.com/.../>.
+F<https://github.com/CpanelInc/Test2-Harness-Renderer-JUnit>.
 
 =head1 MAINTAINERS
 
 =over 4
 
-=item Todd Rinaldo <lt>toddr@cpan.orgE<gt>
+=item Todd Rinaldo <lt>toddr@cpanel.netE<gt>
 
 =back
 
@@ -348,7 +403,7 @@ F<http://github.com/.../>.
 
 =head1 COPYRIGHT
 
-Copyright 2019 Todd Rinaldo<lt>toddr@cpan.orgE<gt>.
+Copyright 2019 Todd Rinaldo<lt>toddr@cpanel.netE<gt>.
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.

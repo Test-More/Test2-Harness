ac87de5be689bf8cf3ec902030ad1da3bf1456b9
Chad Granum <exodist7@gmail.com>
2019-08-28 14:22:12 -0700

    Automated Version Bump

--- lib/Test2/Harness/UI/Util/Errors.pm
+++ lib/Test2/Harness/UI/Util/Errors.pm
@@ -2,7 +2,7 @@ package Test2::Harness::UI::Util::Errors;
 use strict;
 use warnings;
 
-our $VERSION = '0.000015';
+our $VERSION = '0.000016';
 
 use Scalar::Util qw/blessed/;
 
--- scripts/yath-ui-importer.pl
+++ scripts/yath-ui-importer.pl
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 
-our $VERSION = '0.000015';
+our $VERSION = '0.000016';
 
 use Test2::Harness::UI;
 use Test2::Harness::UI::Config;

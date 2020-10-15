139e54a6af5d80b163a0a25d4792d0d66462160b
Chad Granum <exodist7@gmail.com>
2020-10-15 12:30:40 -0700

    Add 'spawn' command
    This command lets you launch new processes from a preloaded yath state.
    Unlike tests these processes are not attached to the harness once they
    have started.
    
    The implementation is tricky... You have a running process (yath), you start a
    new process (spawn) which tells yath to fork and run some code in the
    new process (child). This alone is sane, however you want 'spawn' to
    manage the new process, which is not connected by any parent/child
    relationship.
    
    To make this work:
    
     1) The child needs to know the spawn pid, and uses it to redirect IO to
        the spawn's stdout/stderr using /proc/pid/fd/(1,2)
     2) The child also needs to respond to keyboard input sent to spawn, we
        cannot simply open /proc/pid/fd/0, so instead we re-open STDIN via
        pipe(), then send the pid of the write handle of that pipe back to
        spawn which can now open it using the /proc/pid/fd/N trick.
     3) spawn cannot use wait/waitpid on child because it is not the parent,
        so we have to poll kill(0, $pid) to wait for it to finish
     4) Without waitpid we cannot get the exit status of child, so yath
        actually forks to create a detached watcher process which then forks
        to start child. The watcher process does waitpid on the child, and
        writed the exit code to the IPC file we have used to send pids and
        fileno's around.
     5) While we loop/poll kill(0,pid) we also read from STDIN and forward
        anything on to the pipe the the child processes STDIN handle
     6) We add SIG handlers to several common signals (int, term, etc) that
        forward the signal to the child process.
     7) If the child exits due to a signal we send that same signal to the
        spawn process so it gets the same exit information
     8) The spawn process exits with the same exit code as the child
        process.

---
+++ lib/Test2/Harness/Runner/Spawn.pm
@@ -0,0 +1,89 @@
+package Test2::Harness::Runner::Spawn;
+use strict;
+use warnings;
+
+our $VERSION = '1.000029';
+
+use parent 'Test2::Harness::Runner::Job';
+use Test2::Harness::Util::HashBase;
+
+sub init {
+    my $self = shift;
+
+    $self->{+RUN} //= Test2::Harness::Runner::Spawn::Run->new();
+}
+
+sub out_file { sprintf('/proc/%i/fd/1', $_[0]->{+TASK}->{owner}) }
+sub err_file { sprintf('/proc/%i/fd/2', $_[0]->{+TASK}->{owner}) }
+sub in_file  { undef }
+
+sub args { $_[0]->{+TASK}->{args} //= [] }
+
+sub job_dir { "" }
+sub run_dir { "" }
+
+sub use_stream   { 0 }
+sub event_uuids  { 0 }
+sub mem_usage    { 0 }
+sub io_events    { 0 }
+
+# These return lists
+sub load_import { }
+sub load        { }
+
+package Test2::Harness::Runner::Spawn::Run;
+
+sub new { bless {}, shift };
+
+sub env_vars { {} }
+
+sub AUTOLOAD { }
+
+1;
+
+__END__
+
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Test2::Harness::Runner::Spawn - Minimal job class used for spawning processes
+
+=head1 DESCRIPTION
+
+Do not use this directly...
+
+=head1 SOURCE
+
+The source code repository for Test2-Harness can be found at
+F<http://github.com/Test-More/Test2-Harness/>.
+
+=head1 MAINTAINERS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 AUTHORS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2020 Chad Granum E<lt>exodist7@gmail.comE<gt>.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+See F<http://dev.perl.org/licenses/>
+
+=cut

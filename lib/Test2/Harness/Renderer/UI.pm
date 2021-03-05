c90f27e5f181bff64fe979ef2c2eaad6dec7cb41
Chad Granum <exodist7@gmail.com>
2021-03-05 09:01:53 -0800

    Add UI and UIDB renderers

---
+++ lib/Test2/Harness/Renderer/UI.pm
@@ -0,0 +1,167 @@
+package Test2::Harness::Renderer::UI;
+use strict;
+use warnings;
+
+use Carp qw/croak/;
+
+use Test2::Harness::UI;
+use Test2::Harness::UI::Config;
+use Test2::Harness::UI::RunProcessor;
+
+use Test2::Util qw/pkg_to_file/;
+use Test2::Harness::Util qw/mod2file/;
+use Test2::Harness::UI::Util qw/share_dir dbd_driver qdb_driver/;
+use Test2::Harness::Util::UUID qw/gen_uuid/;
+
+use DBIx::QuickDB;
+use Plack::Builder;
+use Plack::App::Directory;
+use Plack::App::File;
+use Plack::Runner;
+
+use Net::Domain qw/hostfqdn/;
+
+our $VERSION = '0.000035';
+
+use parent 'Test2::Harness::Renderer::UIDB';
+use Test2::Harness::Util::HashBase qw{
+    qdb
+    app
+    links
+    port
+};
+
+#qw{
+#    <run
+#    <processor
+#    <config
+#    <dbh
+#    <project
+#    <user
+#    <finished
+#};
+
+sub init {
+    my $self = shift;
+
+    my $settings = $self->{+SETTINGS};
+
+    my $schema = $settings->yathui->schema // 'PostgreSQL';
+    require(pkg_to_file("Test2::Harness::UI::Schema::$schema"));
+
+    my $db = DBIx::QuickDB->build_db(harness_ui => {driver => qdb_driver($schema), dbd_driver => dbd_driver($schema)});
+    $self->{+QDB} = $db;
+
+    my $dbh = $db->connect('quickdb', AutoCommit => 1, RaiseError => 1);
+    $dbh->do('CREATE DATABASE harness_ui') or die "Could not create db " . $dbh->errstr;
+
+    $db->load_sql(harness_ui => 'share/schema/' . $schema . '.sql');
+
+    my $dsn = $db->connect_string('harness_ui');
+
+    $ENV{HARNESS_UI_DSN} = $dsn;
+
+    my $config = Test2::Harness::UI::Config->new(
+        dbi_dsn     => $dsn,
+        dbi_user    => '',
+        dbi_pass    => '',
+        single_user => 1,
+        single_run  => 1,
+    );
+
+    $self->{+USER} = 'root';
+    my $user = $config->schema->resultset('User')->create({username => 'root', password => 'root', realname => 'root', user_id => gen_uuid()});
+
+    $self->{+PROJECT} = 'default';
+    my $proj = $config->schema->resultset('Project')->create({name => 'default', project_id => gen_uuid()});
+
+    $self->{+CONFIG} = $config;
+
+    my $port = $settings->yathui->port;
+    if (my $cmd = $settings->yathui->port_command) {
+        $cmd =~ s/\$\$/$$/;
+        chomp($port = `$cmd`);
+    }
+    $port //= 8080;
+    $self->{+PORT} = $port;
+
+    $self->{+LINKS} = "\nYathUI:\n  local: http://127.0.0.1:$port\n";
+    if (my $fqdn = hostfqdn()) {
+        $self->{+LINKS} .= "  host:  http://$fqdn:$port\n";
+    }
+    $self->{+LINKS} .= "  DSN:   $dsn\n\n";
+
+    STDOUT->autoflush(1);
+    print $self->{+LINKS};
+
+    $self->{+APP} = $self->start_app();
+
+    $self->SUPER::init();
+}
+
+sub start_app {
+    my $self = shift;
+
+    my $config = $self->{+CONFIG};
+    my $settings = $self->{+SETTINGS};
+
+    my $pid = fork // die "Could not fork: $!";
+    if ($pid) {
+        return $pid;
+    }
+
+    setpgrp(0, 0);
+
+    my $app = builder {
+        mount '/js'  => Plack::App::Directory->new({root => share_dir('js')})->to_app;
+        mount '/css' => Plack::App::Directory->new({root => share_dir('css')})->to_app;
+        mount '/favicon.ico' => Plack::App::File->new({file => share_dir('img') . '/favicon.ico'})->to_app;
+        mount '/img' => Plack::App::Directory->new({root => share_dir('img')})->to_app;
+
+        mount '/' => sub {
+            Test2::Harness::UI->new(config => $config)->to_app->(@_);
+        };
+    };
+
+    $ENV{PLACK_ENV} = 'test';
+    my $r = Plack::Runner->new(access_log => undef, default_middleware => 0);
+    my @options = ("--server", "Starman", '--workers' => 10);
+
+    push @options => ('--listen' => ":" . $self->{+PORT});
+
+    $r->parse_options(@options);
+    open(STDERR, '>', '/dev/null');
+    $r->run($app);
+
+    exit(0);
+}
+
+sub finish {
+    my $self = shift;
+    my $out = $self->SUPER::finish();
+
+    return $out unless kill(0, $self->{+APP});
+
+    my $dsn = $self->{+QDB}->connect_string('harness_ui');
+    print $self->{+LINKS};
+
+    print "Leaving yathui server open, press enter to stop it...\n";
+    my $in = <STDIN>;
+
+    kill('TERM', $self->{+APP});
+    waitpid($self->{+APP}, 0);
+    return $out;
+}
+
+sub DESTROY {
+    my $self = shift;
+
+    if (my $pid = $self->{+APP}) {
+        kill('TERM', $pid);
+        waitpid($pid, 0);
+    }
+
+    $self->SUPER::DESTROY();
+}
+
+1;
---
+++ lib/Test2/Harness/Renderer/UIDB.pm
@@ -0,0 +1,155 @@
+package Test2::Harness::Renderer::UIDB;
+use strict;
+use warnings;
+
+use Carp qw/croak/;
+
+use Test2::Harness::UI::Config;
+use Test2::Harness::UI::RunProcessor;
+
+use Test2::Harness::Util::UUID qw/gen_uuid/;
+
+use Test2::Harness::Util qw/mod2file/;
+
+our $VERSION = '0.000035';
+
+use parent 'Test2::Harness::Renderer';
+use Test2::Harness::Util::HashBase qw{
+    <run
+    <processor
+    <config
+    <dbh
+    <project
+    <user
+    <finished
+};
+
+sub init {
+    my $self = shift;
+
+    my $settings = $self->settings;
+
+    my $yath = $settings->yathui;
+    $self->{+PROJECT} //= $yath->project || die "The yathui-project option is required.\n";
+    $self->{+USER} //= $yath->user || die "The yathui-user option is required.\n";
+
+    my $config = $self->{+CONFIG};
+
+    unless ($config) {
+        my $db = $settings->prefix('yathui-db') or die "No DB settings";
+
+        if (my $cmod = $db->config) {
+            my $file = mod2file($cmod);
+            require $file;
+
+            $config = $cmod->yath_ui_config(%$db);
+        }
+        else {
+            my $dsn = $db->dsn;
+
+            unless ($dsn) {
+                $dsn = "";
+
+                my $driver = $db->driver;
+                my $name   = $db->name;
+
+                $dsn .= "dbi:$driver"  if $driver;
+                $dsn .= ":dname=$name" if $name;
+
+                if (my $socket = $db->socket) {
+                    my $ld = lc($driver);
+                    if ($ld eq 'pg') {
+                        $dsn .= ";host=$socket";
+                    }
+                    else {
+                        $dsn .= ";${ld}_socket=$socket";
+                    }
+                }
+                else {
+                    my $host = $db->host;
+                    my $port = $db->port;
+
+                    $dsn .= ";host=$host" if $host;
+                    $dsn .= ";port=$port" if $port;
+                }
+            }
+
+            $config = Test2::Harness::UI::Config->new(
+                dbi_dsn  => $dsn,
+                dbi_user => $db->user // '',
+                dbi_pass => $db->pass // '',
+            );
+        }
+        $self->{+CONFIG} = $config;
+    }
+
+    $config->connect // die "Could not connect to the db";
+}
+
+sub signal {
+    my $self = shift;
+    my ($sig) = @_;
+
+    $self->{+PROCESSOR}->set_signal($sig) if $self->{+PROCESSOR};
+
+    my $run = $self->{+RUN} or return;
+    $run->update({status => 'canceled', error => "Canceled with signal '$sig'"});
+}
+
+sub render_event {
+    my $self = shift;
+    my ($event) = @_;
+
+    my $f = $event->{facet_data};
+
+    if (my $runf = $f->{harness_run}) {
+        my $run_id = $runf->{run_id} or die "No run-id?";
+
+        my $config = $self->{+CONFIG};
+
+        my $p = $config->schema->resultset('Project')->find_or_create({name => $self->{+PROJECT}, project_id => gen_uuid()});
+        my $u = $config->schema->resultset('User')->find_or_create({username => $self->{+USER}, user_id => gen_uuid(), role => 'user'});
+
+        my $run = $config->schema->resultset('Run')->create({
+            run_id     => $run_id,
+            mode       => $self->settings->yathui->mode,
+            status     => 'pending',
+            user_id    => $u->user_id,
+            project_id => $p->project_id,
+        });
+
+        $self->{+RUN} = $run;
+
+        my $processor = Test2::Harness::UI::RunProcessor->new(
+            config => $config,
+            run => $run,
+        );
+
+        $self->{+PROCESSOR} = $processor;
+        $processor->start();
+    }
+    elsif (!$self->{+RUN}) {
+        die "Run was not seen!";
+    }
+
+    $self->{+PROCESSOR}->process_event($event, $f);
+}
+
+sub finish {
+    my $self = shift;
+
+    $self->{+PROCESSOR}->finish();
+
+    $self->{+FINISHED} = 1;
+}
+
+sub DESTROY {
+    my $self = shift;
+
+    return if $self->{+FINISHED};
+
+    my $run = $self->{+RUN} or return;
+    $run->update({status => 'broken', error => 'Run did not finish'});
+}
+
+1;

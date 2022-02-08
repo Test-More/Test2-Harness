c8f94a6ec713a83ae5994c0b8624244492fb387c
Chad Granum <exodist7@gmail.com>
2022-02-08 10:10:32 -0800

    Fix uninitialized warning

--- lib/Test2/Harness/Finder.pm
+++ lib/Test2/Harness/Finder.pm
@@ -370,7 +370,12 @@ sub changes_from_diff {
             my $maybe_prefix = $1;
             my $maybe_file = $2;
             next if $maybe_file =~ m{/dev/null};
-            $file = -f "$maybe_prefix$maybe_file" ? "$maybe_prefix$maybe_file" : $maybe_file;
+            if ($maybe_prefix) {
+                $file = -f "$maybe_prefix$maybe_file" ? "$maybe_prefix$maybe_file" : $maybe_file;
+            }
+            else {
+                $file = $maybe_file;
+            }
             $is_perl = 1 if $file =~ m/\.(pl|pm|t2?)$/;
             $sub  = '*'; # Wildcard, changes to the code outside of a sub potentially effects all subs
             next;

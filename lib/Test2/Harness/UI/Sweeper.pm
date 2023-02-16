6d2f3c3dd9334f0471a99c704f8b0d2bc8e207ce
Chad Granum <exodist7@gmail.com>
2023-02-16 08:22:38 -0800

    Add resources category to sweeper

--- lib/Test2/Harness/UI/Sweeper.pm
+++ lib/Test2/Harness/UI/Sweeper.pm
@@ -25,6 +25,7 @@ sub sweep {
     $params{jobs}       //= 1;
     $params{coverage}   //= 1;
     $params{events}     //= 1;
+    $params{resources}  //= 1;
     $params{subtests}   //= 1;
     $params{run_fields} //= 1;
     $params{job_fields} //= 1;
@@ -36,7 +37,7 @@ sub sweep {
     $params{jobs} = 0 unless $params{events} && $params{subtests} && $params{job_fields};
 
     # Cannot delete runs if we save jobs or coverage
-    $params{runs} = 0 unless $params{jobs} && $params{coverage} && $params{run_fields};
+    $params{runs} = 0 unless $params{jobs} && $params{coverage} && $params{run_fields} && $params{resources};
 
     my $db_type = $self->config->guess_db_driver;
 
@@ -133,15 +134,16 @@ sub sweep_run {
         $run->update({has_coverage => 0}) unless $params{runs};
     }
 
-    if ($params{runs}) {
-        $run->reportings->delete;
-
+    if ($params{resources}) {
         my $batches = $run->resource_batches;
         while (my $batch = $batches->next) {
             $batch->resources->delete;
             $batch->delete;
         }
+    }
 
+    if ($params{runs}) {
+        $run->reportings->delete;
         $run->sweeps->delete;
         $run->delete;
     }

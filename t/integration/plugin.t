08fffa4d4fc806501bf2993ce4484c358f50ae74
Chad Granum <exodist7@gmail.com>
2026-04-21 03:11:16 -0700

    t/integration: port plugin.t (skip_all with TODO)
    plugin.t exercises a custom TestPlugin with duration_data,
    changed_files, get_coverage_tests, munge_files / munge_search,
    claim_file, and deprecated-hook warnings. Blocked on several
    Stage 6 options (-A / --durations-threshold / --no-plugins /
    --changes-plugin) staying commented-out and on the full hook
    surface not being reflected in Role::Plugin yet. Fixture dir
    (including the local TestPlugin.pm) carried across.

---
+++ t/integration/plugin.t
@@ -0,0 +1,26 @@
+use Test2::V0;
+
+# TODO Stage 18: plugin.t exercises a custom TestPlugin with
+# hooks for duration_data, changed_files, get_coverage_tests,
+# munge_files / munge_search, claim_file, and deprecated-hook
+# warnings. The old plugin used the -p+TestPlugin + -A +
+# --durations-threshold + --no-plugins + --changes-plugin flags.
+#
+# Two classes of gap:
+#
+#   * -A / --durations-threshold / --changes-plugin / --no-plugins
+#     are all commented-out Stage-6 TODO options on
+#     App::Yath2::Options::* ; Stage 6's priority set did not
+#     include them.
+#   * The hook surface the fixture drives (duration_data,
+#     get_coverage_tests, munge_files, claim_file) is a superset
+#     of what Test2::Harness2::Role::Plugin + App::Yath2::Role::
+#     Plugin expose today (Stage 7). The deprecated-hook
+#     warnings path (inject_run_data / handle_event / setup /
+#     teardown) also needs a concrete porting decision -- those
+#     hooks were removed, not kept with a deprecation shim.
+#
+# Fixture dir carried across so the test surface (including the
+# local TestPlugin.pm) is available to the future port.
+
+plan skip_all => "plugin.t needs -A/--durations-threshold/--changes-plugin/--no-plugins options + the full per-plugin hook surface (Stage 6 + Stage 18 follow-up).";
---
+++ t/integration/plugin/lib/App/Yath2/Plugin/TestPlugin.pm
@@ -0,0 +1,137 @@
+package App::Yath2::Plugin::TestPlugin;
+use strict;
+use warnings;
+
+use Test2::Harness2::Util::HashBase qw/-foo/;
+use Test2::Harness2::Util::JSON qw/encode_json/;
+
+use Scalar::Util qw/blessed/;
+
+use parent 'App::Yath2::Plugin';
+
+print "TEST PLUGIN: Loaded Plugin\n";
+
+sub duration_data {
+    my $self = shift;
+
+    print "TEST PLUGIN: duration_data\n";
+
+    return {
+        't/Yath/integration/plugin/a.tx'    => 'short',
+        't/Yath/integration/plugin/b.tx'    => 'medium',
+        't/Yath/integration/plugin/c.tx'    => 'medium',
+        't/Yath/integration/plugin/d.tx'    => 'medium',
+        't/Yath/integration/plugin/test.tx' => 'long',
+    };
+}
+
+sub get_coverage_tests {
+    my $self = shift;
+    my ($settings, $changes) = @_;
+
+    my $stype = ref($settings);
+    my $type = ref($changes);
+    my $count = keys %$changes;
+
+    print "TEST PLUGIN: get_coverage_tests($stype, $type($count))\n";
+
+    return [
+        't/Yath/integration/plugin/a.tx',
+        't/Yath/integration/plugin/b.tx',
+        't/Yath/integration/plugin/c.tx',
+        't/Yath/integration/plugin/d.tx',
+        't/Yath/integration/plugin/test.tx',
+    ];
+}
+
+sub changed_files {
+    my $self = shift;
+    my ($settings) = @_;
+    my $type = ref($settings);
+
+    print "TEST PLUGIN: changed_files($type)\n";
+
+    return (
+        't/Yath/integration/plugin/a.tx',
+        't/Yath/integration/plugin/b.tx',
+        't/Yath/integration/plugin/c.tx',
+        't/Yath/integration/plugin/d.tx',
+        't/Yath/integration/plugin/test.tx',
+    );
+}
+
+sub sort_files_2 {
+    my $self = shift;
+    my %params = @_;
+
+    die "self is not an instance! ($self)" unless blessed($self);
+
+    my $settings = $params{settings} or die "NO SETTINGS!";
+    my $files = $params{files};
+
+    my %rank = (
+        test => 1,
+        c    => 2,
+        b    => 3,
+        a    => 4,
+        d    => 5,
+    );
+
+    my @files = sort {
+        my $an = $a->file;
+        my $bn = $b->file;
+        $an =~ s/^.*\W(\w+)\.tx$/$1/;
+        $bn =~ s/^.*\W(\w+)\.tx$/$1/;
+        $rank{$an} <=> $rank{$bn};
+    } @$files;
+
+    return @files;
+};
+
+sub munge_files {
+    my $self = shift;
+    die "self is not an instance! ($self)" unless blessed($self);
+    print "TEST PLUGIN: munge_files\n";
+    return;
+}
+
+sub munge_search {
+    my $self = shift;
+    die "self is not an instance! ($self)" unless blessed($self);
+    my ($search, $default_search) = @_;
+
+    print "TEST PLUGIN: munge_search\n";
+
+    @$search = ();
+
+    my $path = __FILE__;
+    $path =~ s{lib.{1,2}App.{1,2}Yath.{1,2}Plugin.{1,2}TestPlugin\.pm$}{}g;
+
+    @$default_search = ($path);
+
+    return;
+}
+
+sub claim_file {
+    my $self = shift;
+    die "self is not an instance! ($self)" unless blessed($self);
+    my ($file) = @_;
+    print "TEST PLUGIN: claim_file $file\n";
+
+    if ($file =~ /\.tx/) {
+        require Test2::Harness2::TestFile;
+        return Test2::Harness2::TestFile->new(file => $file);
+    }
+
+    return;
+}
+
+
+sub finish          { die "Should not be called" }
+sub finalize        { die "Should not be called" }
+sub inject_run_data { die "Should not be called" }
+sub handle_event    { die "Should not be called" }
+sub setup           { die "Should not be called" }
+sub teardown        { die "Should not be called" }
+
+1;
